9ce7f585dc1d7a66bafed8f090f7c638b7491f75
Ademílson F. Tonato <ademilsonft@outlook.com>
2018-09-13 17:38:34 -0300

    Install JWT and configure schema and resolver to token

---
+++ src/graphql/resources/token/token.resolvers.ts
@@ -0,0 +1,30 @@
+import * as jwt from 'jsonwebtoken';
+import { DbConnection } from '../../../interfaces/DbConnection.interface';
+import { UserInstance } from '../../../interfaces/User.interface';
+import { JWT_SECRET } from '../../../utils/utils';
+
+export const tokenResolvers = {
+  Mutation: {
+    createToken: (parent, { email, password }, { db }: { db: DbConnection }) => {
+      return db.User.findOne({
+        where: {
+          email,
+          password
+        },
+        attributes: ['id', 'password']
+      }).then((user: UserInstance) => {
+        if (!!user || !user.isPassword(user.get('password'), password)) {
+          throw new Error('Unauthorized, wrong email or password!');
+        }
+
+        const payload = {
+          sub: user.get('id')
+        };
+
+        return {
+          token: jwt.sign(payload, JWT_SECRET)
+        };
+      });
+    }
+  }
+};
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -6,10 +6,12 @@ import { commentResolvers } from './resources/comment/comment.resolvers';
 import { commentTypes } from './resources/comment/comment.schema';
 import { postResolvers } from './resources/post/post.resolvers';
 import { postTypes } from './resources/post/post.schema';
+import { tokenResolvers } from './resources/token/token.resolvers';
+import { tokenTypes } from './resources/token/token.schema';
 import { userResolvers } from './resources/user/user.resolvers';
 import { userTypes } from './resources/user/user.schema';
 
-const resolvers = merge(commentResolvers, postResolvers, userResolvers);
+const resolvers = merge(commentResolvers, postResolvers, tokenResolvers, userResolvers);
 
 const SchemaDefinition = `
   type Schema {
@@ -19,6 +21,6 @@ const SchemaDefinition = `
 `;
 
 export default makeExecutableSchema({
-  typeDefs: [SchemaDefinition, Query, Mutation, commentTypes, postTypes, userTypes],
+  typeDefs: [SchemaDefinition, Query, Mutation, commentTypes, postTypes, tokenTypes, userTypes],
   resolvers: [resolvers]
 });
